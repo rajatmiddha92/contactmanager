5c3077163f88b17388453145c789e84f4dada944
Rajat Middha <rajatmiddha92@gmail.com>
2023-02-07 14:59:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const conn = require("./connection/conn");
 const cors = require("cors");
-const registerRoute = require("./routes/register");
 const addContact=require('./routes/addcontacts')
 const deleteContact=require('./routes/deletecontact')
+const registerRoute = require("./routes/register");
+const logInRoute = require('./routes/Login')
 const app = express();
 let port = 5500;
 conn();
 
+const fileUploader =  require("express-fileupload")
+//Midlewares
+app.use(fileUploader())
 app.use(cors());
 app.use(registerRoute);
+app.use(logInRoute)
 app.use(addContact)
 app.use(deleteContact)
-
 
 app.listen(port, () => console.log(`app running on port ${port}`));

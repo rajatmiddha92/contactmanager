152f5150f6ea014cddb71da51d60416eaefecae5
Rajat <rajatmiddhaw92@gmail.com>
2023-02-07 13:32:37 +0530

    delete contact api and add contact api implemented

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const conn = require("./connection/conn");
 const cors = require("cors");
 const registerRoute = require("./routes/register");
+const addContact=require('./routes/addcontacts')
+const deleteContact=require('./routes/deletecontact')
 const app = express();
 let port = 5500;
 conn();
 
 app.use(cors());
 app.use(registerRoute);
+app.use(addContact)
+app.use(deleteContact)
+
 
 app.listen(port, () => console.log(`app running on port ${port}`));

cdf399bbb781f6a89661872f0d9697ecbeda76b0
Rajat <rajatmiddhaw92@gmail.com>
2023-02-08 16:21:23 +0530

    remaining components added

--- frontend/src/components/DeleteSuccessfullyFile.jsx
+++ frontend/src/components/DeleteSuccessfullyFile.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import {blue } from '@mui/material/colors';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const DeleteSuccessfullyFile=(props)=> {
     return (
         <>
+        <div className='container-2'>
+        <section className='box-2'>
+          <CheckCircleIcon sx={{ fontSize: 40,color:blue[400] }}  className='sucess' />
+          <h4 className='text-head-2'>Deleted Contacts</h4>
+        </section>
+      </div>
         </>
     );
 }
